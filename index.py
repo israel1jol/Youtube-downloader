7337c60f5717265f8ec6fd545724b6e5f472f08f
israel <adigun.jolly@gmail.com>
2022-03-21 23:18:46 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+import sys
+
+link = sys.argv[-1]
+if(type(link) == str and link[0:31] == "https://www.youtube.com/watch?v"):
+    print("ok")
+
+
+# yt = YouTube(link)
+# ys = yt.streams.get_highest_resolution()
+
+# print("Downloading...")
+# ys.download()
+# print("Video downloaded")
